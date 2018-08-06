2fce2bbb2dc272244655aac286bb9ec9924fd3ec
Sergey Khmelevskoy <xs290@me.com>
2018-08-06 16:17:16 +0300

    added rest of dash welcome sections, responsive

---
+++ src/components/Alert.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+  constructor(props){
+    super(props)
+
+    let getColor
+    switch (props.color) {
+      case "blue":
+        getColor = "#E1F1FF"
+        break;
+      default:
+        getColor = "#E1F1FF"
+    }
+
+    this.color = getColor
+  }
+
+  render(){
+
+    return(
+      <div className="alert" style={{"background-color": this.color}}>
+        {this.props.content}
+      </div>
+    )
+  }
+}
+
+export default Alert
