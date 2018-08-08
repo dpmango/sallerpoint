b1ca15219d55ceb2077212e468995c742ed38a89
Sergey Khmelevskoy <xs290@me.com>
2018-08-08 20:40:06 +0300

    withCredentials: true

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,15 +4,17 @@ import store from '../store/store';
 // let BACKEND_URL = process.env.NODE_ENV === 'production' ? "http://name.herokuapp.com" : "http://localhost:8000/"
 let BACKEND_URL = "http://qa.kinimetrix.com:8082/SellerPoint/"
 
+axios.defaults.withCredentials = true;
+
 const api = axios.create({
   baseURL: BACKEND_URL,
   headers: {
     'authToken': store.getState().login.authToken,
-    // 'withCredentials': true,
     'Content-Type': 'application/json',
     'Accept': 'application/json',
     'X-Requested-With': 'XMLHttpRequest'
-  }
+  },
+  withCredentials: true
 });
 
 export default api;
