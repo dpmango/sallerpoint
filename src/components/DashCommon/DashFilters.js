cedd1c72d601324b8ae1314e1921718160084f65
Praveena Mundolimoole <pmundolimoole@thekinigroup.com>
2018-08-15 08:36:59 -0700

    React DatePicker Experiment

--- src/components/DashCommon/DashFilters.js
+++ src/components/DashCommon/DashFilters.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import QdtComponent from '../Qlik/QdtComponent';
+import DateRangePicker from 'react-bootstrap-daterangepicker';
+//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-daterangepicker/daterangepicker.css';
+
+import moment from 'moment';
 
 export default class DashFilters extends Component {
+    handleEvent(event, picker) {
+        if (event.type == 'apply') { 
+            debugger;
+        let a = window.qlik.currApp();
+        a.field('Date').selectMatch('>=' + picker.startDate._i + '<=' + picker.endDate._i, true);
+    }
+    }
+    
     render() {
 
         // TODO - not rendering - wrong type or/and props ?
+      const  ranges= {
+            'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                            'This Month': [moment().startOf('month'), moment().endOf('month')],
+                                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+      }
         const filters = [
             {
                 name: "DataGroupedBy",
@@ -15,15 +36,15 @@ export default class DashFilters extends Component {
                     },
                 }
             },
-           {
-                name: "DatePicker",
-                qdt: {
-                    type: 'QdtViz',
-                    props: {
-                        id: 'JQfpVS', height: '40px',width:'400px'
-                    },
-                }
-            },
+           //{
+           //     name: "DatePicker",
+           //     qdt: {
+           //         type: 'QdtViz',
+           //         props: {
+           //             id: 'JQfpVS', height: '40px',width:'400px'
+           //         },
+           //     }
+           // },
             {
                 name: "SellerId",
                 qdt: {
@@ -67,7 +88,20 @@ export default class DashFilters extends Component {
                                 />
                             )
                         })}
+                        <div>
+                            <DateRangePicker onEvent={this.handleEvent} startDate="8/1/2018" endDate="8/18/2018" ranges={ranges} containerClass="react-bootstrap-daterangepicker-container"> 
+                            <div className="input-group">
+                                <input type="text" className="form-control" value="" />
+                                <span className="input-group-btn">
+                                    <button className="default date-range-toggle">
+                                        <i className="fa fa-calendar" />
+                                    </button>
+                                </span>
+                            </div>
+                        </DateRangePicker>
                     </div>
+                    </div>
+                   
                 </div>
             </div>
         )
